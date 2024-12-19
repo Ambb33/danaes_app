e164c7318ab4954240ca806ebcb5407b7c59cf78
Andreea Mocan <andreeamocan89@gmail.com>
2024-12-19 13:58:58 +0100

    more adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import Menu from '@/components/Menu';
+import { MyContextProvider } from '../context/MyContextProvider';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -31,14 +32,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background`}
       >
-        <div className="flex flex-col md:flex-row min-h-screen">
-          <nav >
-            <Menu />
-          </nav>
-          <main className="flex-1 p-4 bg-center">
-            {children}
-          </main>
-        </div>
+        <MyContextProvider>
+          <div className="flex flex-col md:flex-row min-h-screen">
+            <nav>
+              <Menu />
+            </nav>
+            <main className="flex-1 p-4 bg-center">
+              {children}
+            </main>
+          </div>
+        </MyContextProvider>
       </body>
     </html>
   );
